6aa121428a79a479797d24f50473f79a52a01e42
Felix Yu <felix.yu@stryker.com>
2023-07-31 15:17:39 -0400

    Added get-item axios

--- webserver.py
+++ webserver.py
@@ -1,9 +1,16 @@
+import threading
 from flask import Flask, render_template, request, session, jsonify
 from flask_cors import CORS, cross_origin
-from flask_session import Session
-#import rclpy
+# from flask_session import Session
+from collections import deque # used for queuing tasks
+
+# from rclpy.node import Node
+# import rclpy
+# from std_msgs.msg import Int32
 import secrets
 
+
+# settings for sessions
 app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
@@ -13,19 +20,32 @@ app.config.update(
 app.secret_key = secrets.token_hex(32)
 
 # enable CORS
-Session(app)
+# Session(app)
 CORS(app)
 
+# for ROS - comment out unless working on robot computer
 # class ROSNode(Node):
 #     def __init__(self):
-#         super().__init__('flask_ros_node')
-#         self.publisher = self.create_publisher(String, 'ros_topic', 10)
-# setup a rate to send messages sync the hz between the sent and the rates
-#     def publish_message(self, message):
-#         msg = String()
-#         msg.data = message
+#         super().__init__('ROS_node')
+#         self.publisher = self.create_publisher(Int32, 'topic', 10)
+# # setup a rate to send messages sync the hz between the sent and the rates
+#     def send_int_location(self, location):
+#         msg = Int32()
+#         msg.data = location
 #         self.publisher.publish(msg)
-#         self.get_logger().info(f"Message published to ROS: {message}")
+#         self.get_logger().info(f"Location sent to ROS {msg}")
+
+# def ros_spin(node):
+#     rclpy.spin(node)
+
+# def main(args=None):
+#     rclpy.init(args=args)
+#     global Node
+#     node = ROSNode()
+#     t = threading.Thread(target=ros_spin, args=(node,))
+#     t.start()
+#     app.run(debug=True)
+
 @app.route('/')
 def hello():
     return render_template('index.html')
@@ -42,76 +62,89 @@ def defineRobotLocation(location):
 
 def sendDataToROS(location):
     # label based on recieve
-
     # int or xyz or string, 
     # from the payload get data
-    # # ROS Communication
+    # ROS Communication
     # rclpy.init()
     # node = ROSNode()
+    # robotLocation = defineRobotLocation(location)
     # node.publish_message(robotLocation)
     # rclpy.shutdown()
     print("Data sent to ROS\n")
     return
 
+# data store - used instead of session because it is not working
+data_store_locations = deque()
+data_store_items = deque()
+
 @app.route('/request', methods=['POST', 'OPTIONS'])
 @cross_origin(supports_credentials=True)
 def requestReceived():
-    # if request.method == 'OPTIONS':
-    #     # Preflight request. Reply successfully:
-    #     resp = app.make_default_options_response()
-
-    #     # Allow CORS on this route:
-    #     headers = resp.headers
-    #     headers['Access-Control-Allow-Origin'] = '*'
-    #     headers['Access-Control-Allow-Headers'] = 'Authorization, Content-Type'
-    #     headers['Access-Control-Allow-Methods'] = 'GET, POST'
-
-    #     return resp
     if request.method == 'POST':
         startLocation = request.json.get('start_location')
         endLocation = request.json.get('end_location')
         itemToSend = request.json.get('item')
+
+        print(request.json)
         sendDataToROS(startLocation)
-        print(f'Sent location data to ROS: {startLocation}')
+
         data = {
             'item': itemToSend
         }
-        if 'locations_list' not in session:
-            session['locations_list'] = []  # Create a new list if it doesn't exist in the session
-        session['locations_list'].append(endLocation)  # Add the end location to the queue
+        # if 'locations_list' not in session:
+        #     session['locations_list'] = []  # Create a new list if it doesn't exist in the session
+        # session['locations_list'].append(endLocation)  # Add the end location to the queue
+        data_store_items.append(itemToSend)
+        data_store_locations.append(endLocation)
+
         # add vue to the route and then route to the html file that has the progress bar
-        # connect this with a jinja template that is the same as the vue template
         print('Request received successfully')
-        print(session['locations_list'])
+        # print(session['locations_list'])
         return jsonify(data)
 
-# create an axios request that sends this information to the backend server
-# worst case scenario, hard code it
 @app.route('/send', methods=['POST'])
 @cross_origin(supports_credentials=True)
 def returnButtonPushed():
-    print(session)
     data = {'locationSent': False}
-    location = 'None'
-    if 'location_list' in session:
-        locations_list = session.get('locations_list', [])
-        print(locations_list)
-        location = locations_list[0] if locations_list else None   # Retrieve the location from the queue
-        data['location'] = True
-        # remove location from location list and send it to ROS
-        locations_list.pop(0)
+    location = ''
+    # if 'location_list' in session:
+    #     locations_list = session.get('locations_list', [])
+    #     print(locations_list)
+    #     location = locations_list[0] if locations_list else None   # Retrieve the location from the queue
+    #     data['location'] = True
+    #     # remove location from location list and send it to ROS
+    #     locations_list.pop(0)
+    #     sendDataToROS(location)
+    #     print(f'Sent data succesfully {location}')
+    if len(data_store_locations) > 0:
+        location = data_store_locations.popleft()
         sendDataToROS(location)
+        data['location'] = True
         print(f'Sent data succesfully {location}')
-        session.modified = False
     else:
         print('No locations available to send to robot')
-    print(session)
     return jsonify(data)
 
-# create an axios requests that sends data to progress bar via a get request
-# best way is to grab from the robot, for now it is hardcoded
+# create an axios requests that sends location data to progress bar via a get request
+# for now, 0 if request hasn't been queued, and 1 if request is been completed on the send side
 @app.route('/location', methods=['GET'])
 def sendLocationStatus():
-    data = {'location': 1}
+    # location = ''
+    location = 0
+    if len(data_store_locations) == 0:
+        # location = data_store_locations.popleft()
+        location = 1
+    # change robot location to string accordingly
+    data = {'location': location}
     # make a call to robot to get it's current location
     return jsonify(data)
+
+# write get item function
+@app.route('/get-item', methods=['GET'])
+def getItem():
+    # grab item from the store if it exists, otherwise return nothing
+    item = 'None' if (len(data_store_items) == 0) else data_store_items.pop()
+    print(item)
+    itemJson = {'item': item}
+    return jsonify(itemJson)
+
